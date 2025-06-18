faaa68849ffb9458728c08c58c6d30360e1dd589
ZAINMUMTAZ999 <mzainmumtaz99@gmail.com>
2025-06-19 01:19:22 +0500

    wwasssssssssssssss

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -83,7 +83,7 @@ const Services = () => {
 
 
   return (
-  <div className="min-h-screen -mt-24 bg-white">
+  <div className="min-h-screen -mt-16 bg-white">
       {/* Main Services Section */}
       <section className="py-20 lg:py-32">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
