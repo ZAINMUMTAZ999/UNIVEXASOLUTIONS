758767cd0a0db58d757050838d8e49c02a01b545
ZAINMUMTAZ999 <mzainmumtaz99@gmail.com>
2025-06-21 21:00:11 +0500

    css

--- src/components/services/WebApp.tsx
+++ src/components/services/WebApp.tsx
@@ -6,7 +6,7 @@ import {
   SiMongodb,
   SiExpress,
   SiReact,
-  SiNextdotjs,
+//   SiNextdotjs,
  
   SiNodedotjs,
   SiTypescript,
@@ -14,6 +14,7 @@ import {
   SiNextui,
   SiTailwindcss
 } from "react-icons/si";
+import { CheckCircle2 } from 'lucide-react';
 
 // Define a type for a single technology object for better type safety
 interface Technology {
